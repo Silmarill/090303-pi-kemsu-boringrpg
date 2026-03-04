9a440f450e61c5ef04d8285e53efdc8e451497e3
mrsilmar <mrsilmar@gmail.com>
2026-03-04 17:47:38 +0700

    Skeleton project prepared

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BoringRPG {
+  internal class Program {
+    static void Main(string[] args) {
+      string critText;
+      int beforeHP, damage;
+      
+      DummyClass lancelot = new DummyClass("Ланселот Ловкий");
+      DummyClass artur =    new DummyClass("Артур Пендрагон");
+            
+      Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
+                        $"==================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{artur.GetInfo()}\n");
+           
+      Console.WriteLine($"{lancelot.Name} атакует {artur.Name}!");
+
+      beforeHP = artur.HP;
+      lancelot.Hit(artur);
+      damage = beforeHP - artur.HP;
+
+      critText = lancelot.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+
+      Console.WriteLine($"Нанесено {damage} урона{critText}\n");
+                        
+      Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
+      Console.WriteLine("======================");
+      Console.WriteLine(lancelot.GetInfo());
+      Console.WriteLine(artur.GetInfo());
+      Console.ReadKey();
+    }
+  }
+}
