03dc7178ff533404d211c8179de29dc9e7c687a0
wacper <wacpercs@gmail.com>
2026-03-06 09:05:46 +0700

    Deleted some symbols

--- berserkerClass.cs
+++ berserkerClass.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BoringRPG
-{
+namespace BoringRPG {
   internal class BerserkerClass : Archetype {
     private static Random random = new Random();
     private bool LastHitWasCrit;
