e0932e1545d6b56866ab6cf347d2f0c17abb6a64
Asverktus <aleksandr.rubin123@gmail.com>
2026-03-06 13:06:54 +0700

    Хотябы сделал перса и хит прописал

---
+++ HunterClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BoringRPG
+{
+    internal class HunterClass : Archetype {
+        //пример для работы со случайными числами
+        private static Random random = new Random();
+        public bool LastHitWasCrit;
+
+        public HunterClass(string name, int hp, int mp, int ammo, int dmg, double crit) : base(name, 85, 20, 15, 25, 0.2)
+        {
+        }
+
+        public HunterClass(string name) : base(name, 85, 20, 15, 25, 0.2)
+        {
+        }
+
+        public override void Hit(Archetype target)
+        {
+            int damage = Damage;
+
+            // Метод NextDouble() - возвращает double в диапазоне [0.0; 1.0)
+            LastHitWasCrit = random.NextDouble() < CritChance;
+
+            if (LastHitWasCrit)
+            {
+                damage *= 2;
+            }
+
+            if (HP < target.HP)
+            {
+                damage += 10;
+            }
+
+            Ammo -= 1;
+            target.HP -= damage;
+        }
+
+        public override string GetInfo()
+        {
+            return $"{Name} (Dummy): HP {HP}, MP {MP}, Ammo {Ammo}, Шанс крита {CritChance * 100}%";
+        }
+    }
+}
