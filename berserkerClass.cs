63ae13e81c78ee68453477553bb4a01b69842e5b
wacper <wacpercs@gmail.com>
2026-03-06 08:57:42 +0700

    berserkerClass.cs: Rename file

---
+++ berserkerClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG
+{
+  internal class berserkerClass : Archetype {
+    private static Random random = new Random();
+    private bool LastHitWasCrit;
+
+    public berserkerClass(string name, int hp, int mp, int ammo, int dmg, double crit) : base(name, 100, 50, 10, 20, 0.3) {
+     }
+
+    public berserkerClass(string name) : base(name, 100, 50, 10, 20, 0.3) {
+     }
+    public override void Hit(Archetype target) {
+      int damage = Damage;
+      
+      LastHitWasCrit = random.NextDouble() < CritChance;
+      
+      if (LastHitWasCrit) {
+        damage *= 2;
+      }
+
+      target.HP -= damage;
+    }
+    public override string GetInfo() {
+      return $"{Name} (Dummy): HP {HP}, MP {MP}, Ammo {Ammo}, Шанс крита {CritChance * 100}%";
+    }
+   }
+}
