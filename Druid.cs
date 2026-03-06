e6856596c00d491b72b23d1bdb623294e3d06bdc
Lolkek <molochnoelol@gmail.com>
2026-03-06 09:19:01 +0700

    class

---
+++ Druid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoringRPG {
+  internal abstract class Archetype {
+
+    public string Name;
+    public int HP;
+    public int MP;
+    public int Ammo;
+    public int Damage;
+    public double CritChance;
+
+    protected Archetype(string name, int hp, int mp, int ammo, int dmg, double crit) {
+      Name = name;
+      HP = hp;
+      MP = mp;
+      Ammo = ammo;
+      Damage = dmg;
+      CritChance = crit;
+    }
+
+    public abstract void Hit(Archetype target);
+    public abstract string GetInfo();
+  }
+
+}
