27465ce24bf9f7d9adf85a04d8fe7ff883343e6f
RudDavid <satorugodze17@gmail.com>
2026-03-06 09:12:07 +0700

    pypypy1

---
+++ Hunter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG
+{
+    internal class Hunter : Archetype
+    {
+
+        protected Hunter(string name, int hp, int mp, int ammo, int dmg, double crit)
+            : base("Divad", 85, 20, 15, 25, 0.2)
+        {
+            public private int operator -(int hitPoint, int dephens, int damage)
+        {
+
+            if (HP > 0)
+            {
+                hitPoint -= (Damage - Ammo);
+                if (HP < 0)
+                {
+                    return hitPoint = 0;
+                }
+            }
+            return hitPoint;
+
+        }
+    }
+
+}
+}
