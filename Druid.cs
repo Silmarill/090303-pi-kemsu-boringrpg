3a75050b6383cf9c1b6e8e7dd3b47d555685d30c
Egor12312 <uegor6705@gmail.com>
2026-03-06 14:33:14 +0700

    добавил часть кода

---
+++ Druid.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BoringRPG {
+  internal class Druid : Archetype {
+
+    private static Random random = new Random();
+    public bool LastHitWasCrit;
+
+    public Druid(string name) : base(name, 90, 60, 0, 20, 0.10)
+    {
+    }
+
+    public static Druid operator +(Druid druid, int amount)
+    {
+      druid.HP += amount;
+      return druid;
+    }
+
+    public static Druid operator -(Druid druid, int amount)
+    {
+      druid.HP -= amount;
+      return druid;
+    }
+
+    public static bool operator true(Druid druid)
+    {
+      return druid.HP > 0;
+    }
+
+    public static bool operator false(Druid druid)
+    {
+      return druid.HP <= 0;
+    }
+
+    public override void Hit(Archetype target)
+    {
+
+    }
+
+  
