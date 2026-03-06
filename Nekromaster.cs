650db4ae26898737578facee574f251584dba95a
Vova200804 <sanek_sanek_24@mail.ru>
2026-03-06 09:23:37 +0700

    Структура кода есть, доработать перегрузку, ну и в целом весь код

--- Nekromaster.cs
+++ Nekromaster.cs
@@ -6,9 +6,10 @@ using static System.Net.Mime.MediaTypeNames;
 namespace BoringRPG {
     internal class Nekromaster : Archetype {
         private static Random random = new Random();
-        private int _skeletonBonus = 0;
+        private int _skeletonBonus;
         public bool LastHitWasCrit;
 
+        _skeletonBonus = 0;
         public Nekromaster(string name, int hp, int mp, int ammo, int dmg, double crit) : base (name, 55, 90, 0, 30, 0.1) {
         
         }
@@ -21,31 +22,44 @@ namespace BoringRPG {
             necromancer.HP += amount;
             return necromancer;
         }
-
-        // Перегрузка оператора -
         public static Nekromaster operator -(Nekromaster necromancer, int amount)
         {
             necromancer.HP -= amount;
             return necromancer;
         }
-        public override void Hit(Archetype target)
+        public override void Hit(Nekromaster target)
         {
-            int damage = 30;
+            if (MP < 15)
+            {
+                LastHitWasCrit = false;
+                return;
+            }
 
-            // Метод NextDouble() - возвращает double в диапазоне [0.0; 1.0)
-            LastHitWasCrit = random.NextDouble() < 0.1;
+            MP -= 15;
 
+            int damage = Damage;
+
+            LastHitWasCrit = random.NextDouble() < CritChance;
             if (LastHitWasCrit)
             {
                 damage *= 2;
             }
 
-            target.HP -= damage;
+            damage += _skeletonBonus;
+
+            int targetHpBefore = target.HP;
+
+            target -= damage;
+
+            if (target.HP < targetHpBefore)
+            {
+                _skeletonBonus += 5;
+            }
         }
 
         public override string GetInfo()
         {
-            return $"{Name} (Dummy): HP {HP}, MP {MP}, Ammo {Ammo}, Шанс крита {CritChance * 100}%";
+            return $"{Name} (Некромант): HP {HP}/55, MP {MP}/90, Damage {Damage}, Crit {CritChance * 100}%, SkeletonBonus +{_skeletonBonus}";
         }
     }
 }
