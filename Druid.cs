89d99c510d237b6474229fe0dcad988f5c75fa38
AssistSPB <ungefugnikita@gmail.com>
2026-03-06 12:53:08 +0700

    add paladin

---
+++ Druid.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace BoringRPG {
+  internal class Druid : Archetype {
+    private static Random random = new Random();
+    public bool LastHitWasCrit;
+
+    public Druid(string name) : base(name, 90, 60, 0, 20, 0.10) {
+    }
+
+    public static Druid operator +(Druid druid, int healing) {
+      druid.HP += healing;
+      return druid;
+    }
+
+    public static Druid operator -(Druid druid, int damage) {
+      druid.HP -= damage;
+      return druid;
+    }
+
+    public override void Hit(Archetype target) {
+      int damage = Damage;
+
+      LastHitWasCrit = random.NextDouble() < CritChance;
+
+      if (LastHitWasCrit) {
+        damage *= 2;
+      }
+
+      // тратит 5 МР
+      // eсли цель имеет больше 50% HP - урон удваивается
+      if (MP >= 5) {
+        MP -= 5;
+        if (target.HP < 45) {
+          damage *= 2;
+        }
+      }
+
+      target.HP -= damage;
+    }
+
+    public override string GetInfo() {
+      return $"{Name} (Druid): HP {HP}, MP {MP}, Ammo {Ammo}, Урон {Damage}, Крит {CritChance * 100}%";
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -5,28 +5,28 @@ namespace BoringRPG {
     static void Main(string[] args) {
       string critText;
       int beforeHP, damage;
-      
-      DummyClass lancelot = new DummyClass("Ланселот Ловкий");
-      DummyClass artur =    new DummyClass("Артур Пендрагон");
-            
+
+      Druid name = new Druid("Ацилот");
+      Druid artur = new Druid("Артур Пендрагон");
+
       Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
                         $"==================\n" +
-                        $"{lancelot.GetInfo()}\n" +
+                        $"{name.GetInfo()}\n" +
                         $"{artur.GetInfo()}\n");
-           
-      Console.WriteLine($"{lancelot.Name} атакует {artur.Name}!");
+
+      Console.WriteLine($"{name.Name} атакует {artur.Name}!");
 
       beforeHP = artur.HP;
-      lancelot.Hit(artur);
+      name.Hit(artur);
       damage = beforeHP - artur.HP;
 
-      critText = lancelot.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+      critText = name.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
 
       Console.WriteLine($"Нанесено {damage} урона{critText}\n");
-                        
+
       Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
       Console.WriteLine("======================");
-      Console.WriteLine(lancelot.GetInfo());
+      Console.WriteLine(name.GetInfo());
       Console.WriteLine(artur.GetInfo());
       Console.ReadKey();
     }
