43be3cff9820de6463e1d131dbe7b4612b8af83b
Wergon74 <satorugodze17@gmail.com>
2026-03-07 18:08:02 +0700

    перегрузка + и -, метод GetInfo

--- Hunter.cs
+++ Hunter.cs
@@ -4,29 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BoringRPG
-{
-    internal class Hunter : Archetype
-    {
+namespace BoringRPG {
+  internal class Hunter : Archetype {
 
-        protected Hunter(string name, int hp, int mp, int ammo, int dmg, double crit)
-            : base("Divad", 85, 20, 15, 25, 0.2)
-        {
-            public private int operator -(int hitPoint, int dephens, int damage)
-        {
+    protected Hunter(string name, int hp, int mp, int ammo, int dmg, double crit)
+        : base("Divad", 85, 20, 15, 25, 0.2) { }
 
-            if (HP > 0)
-            {
-                hitPoint -= (Damage - Ammo);
-                if (HP < 0)
-                {
-                    return hitPoint = 0;
-                }
-            }
-            return hitPoint;
+    public static Hunter operator +(Hunter man, int regain) {
 
+      if (man.HP < 85) {
+        man.HP = man.HP + regain;
+        if (man.HP > 85) {
+          man.HP = 85;
+          return man;
         }
+      }
+      return man;
+
     }
 
+    public static Hunter operator -(Hunter man, int damage) {
+
+      if (man.HP > 0) {
+        man.HP = man.HP - (damage - man.Ammo);
+        if (man.HP < 0) {
+          man.HP = 0;
+          return man;
+        }
+      }
+      return man;
+
+    }
+
+
+
+    public override string GetInfo() {
+      return $"{Name} (Hunter): HP {HP}, MP {MP}, Ammo {Ammo}, Crit Chance: {CritChance * 100}%";
+    }
+  }
+
 }
-}
+
