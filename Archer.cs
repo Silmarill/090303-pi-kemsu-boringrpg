f80b3ba0e163237cb6489b39c6c37a889c85d569
inkoromi21 <sema.vorobev20078@gmail.com>
2026-03-07 20:16:56 +0700

    I've done madness

--- Archer.cs
+++ Archer.cs
@@ -33,6 +33,32 @@ namespace BoringRPG
       return myArcher.HP <= 0;
     }
 
+    public int RandomEvent(Archer myArcher)
+    {
+      int randomInt;
+      int randomNumberHP;
+
+      randomInt = random.Next(0, 3);
+      randomNumberHP = random.Next(-20, 21);
+
+      if (randomNumberHP > 0)
+      {
+        myArcher = myArcher + randomNumberHP;
+
+        return randomNumberHP;
+      }
+      else if (randomNumberHP < 0)
+      {
+        myArcher = myArcher - randomNumberHP;
+
+        return randomNumberHP;
+      }
+      else
+      {
+        return randomNumberHP;
+      }
+    }
+
     public override void Hit(Archetype target)
     {
       int damage;
@@ -43,32 +69,22 @@ namespace BoringRPG
       if (LastHitWasCrit)
       {
         damage *= 2;
-
       }
 
       if (Ammo > 0)
       {
         --Ammo;
-        Console.WriteLine($"{Name} took a shot, number of arrows: {Ammo}");
       }
       else
       {
         damage = 0;
-        Console.WriteLine($"{Name} has no arrows. Damage = 0");
       }
 
-      if (target is Archer archerTarget)
-      {
-        archerTarget = archerTarget - damage;
-      }
-      else
-      {
-        target.HP -= damage;
-      }
+      target.HP -= damage;
     }
 
     public override string GetInfo()
-    { 
+    {
       return $"{Name} (Archer): HP: {HP}, MP: {MP}, Ammo: {Ammo}, Crit chance: {CritChance * 100}%";
     }
   }
--- Program.cs
+++ Program.cs
@@ -1,48 +1,101 @@
 ﻿using System;
 using System.Security.Cryptography;
+using System.Xml.Linq;
 
-namespace BoringRPG {
-  internal class Program {
-    static void Main(string[] args) {
+namespace BoringRPG
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
       string critTextLancelot, critTextInkoromi;
-      int beforeHPInkoromi, damageLancelot, beforeHPLancelot, damageInkoromi;
-      
+      int beforeHPInkoromi, damageLancelot, beforeHPLancelot, damageInkoromi, roundNumber, randomEventmeaning;
+
+      roundNumber = 1;
+
       DummyClass lancelot = new DummyClass("Lancelot the Nimble");
-      DummyClass artur =    new DummyClass("Arthur Pendragon");
       Archer archer = new Archer("inkoromi21");
-            
-      Console.WriteLine($"THE BEGINNING OF THE BATTLE. Initial state: \n" +
+
+      randomEventmeaning = archer.RandomEvent(archer);
+
+      Console.WriteLine($"THE BEGINNING OF THE GAME. Initial state: \n" +
                         $"==================\n" +
                         $"{lancelot.GetInfo()}\n" +
-                        $"{artur.GetInfo()}\n" +
                         $"{archer.GetInfo()}\n");
 
-      Console.WriteLine($"{lancelot.Name} attacks {archer.Name}!");
+      if (randomEventmeaning > 0)
+      {
+        Console.WriteLine($"{archer.Name} finds a health potion! +{randomEventmeaning} HP");
+      }
+      else if (randomEventmeaning < 0)
 
-      beforeHPInkoromi = archer.HP;
-      lancelot.Hit(archer);
-      damageLancelot = beforeHPInkoromi - archer.HP;
+      {
+        Console.WriteLine($"{archer.Name} stepped into a trap! {randomEventmeaning} HP");
+      }
+      else
+      {
+        Console.WriteLine($"{archer.Name} calmly reached the enemy");
+      }
 
-      critTextLancelot = lancelot.LastHitWasCrit ? " - CRITICAL HIT!" : "";
+      while (archer)
+      {
+        if (lancelot.HP <= 0)
+        {
+          Console.WriteLine("\nGAME END!\n" + "======================");
+          Console.WriteLine($"{archer.Name} wins!");
 
-      Console.WriteLine($"Damage dealt {damageLancelot} {critTextLancelot}\n");
+          break;
+        }
 
-      Console.WriteLine($"{archer.Name} attacks {lancelot.Name}!");
+        Console.WriteLine($"\nROUND {roundNumber}:\n" + "======================");
 
-      beforeHPLancelot = lancelot.HP;
-      archer.Hit(lancelot);
-      damageInkoromi = beforeHPLancelot - lancelot.HP;
+        Console.WriteLine($"{lancelot.Name} attacks {archer.Name}!");
 
-      critTextInkoromi = archer.LastHitWasCrit ? " - CRITICAL HIT!" : "";
+        beforeHPInkoromi = archer.HP;
+        lancelot.Hit(archer);
+        damageLancelot = beforeHPInkoromi - archer.HP;
 
-      Console.WriteLine($"Damage dealt {damageInkoromi} {critTextInkoromi}\n");
+        critTextLancelot = lancelot.LastHitWasCrit ? " - CRITICAL HIT!" : "";
 
-      Console.WriteLine("FINAL STATE:");
-      Console.WriteLine("======================");
-      Console.WriteLine(lancelot.GetInfo());
-      Console.WriteLine(artur.GetInfo());
-      Console.WriteLine(archer.GetInfo());
-      Console.ReadKey();
+        Console.WriteLine($"Damage dealt {damageLancelot} {critTextLancelot}\n");
+
+        if (archer.HP <= 0)
+        {
+          Console.WriteLine("\nGAME END!\n" + "======================");
+          Console.WriteLine($"{lancelot.Name} wins!");
+
+          break;
+        }
+
+        Console.WriteLine($"{archer.Name} attacks {lancelot.Name}!");
+
+        beforeHPLancelot = lancelot.HP;
+        archer.Hit(lancelot);
+        damageInkoromi = beforeHPLancelot - lancelot.HP;
+
+        if (damageInkoromi > 0)
+        {
+          Console.WriteLine($"{archer.Name} took a shot, number of arrows: {archer.Ammo}");
+        }
+        else
+        {
+          Console.WriteLine($"{archer.Name} has no arrows. Damage = 0");
+        }
+
+        critTextInkoromi = archer.LastHitWasCrit ? " - CRITICAL HIT!" : "";
+
+        Console.WriteLine($"Damage dealt {damageInkoromi} {critTextInkoromi}\n");
+
+        Console.WriteLine("FINAL STATE:");
+        Console.WriteLine("======================");
+        Console.WriteLine(lancelot.GetInfo());
+        Console.WriteLine(archer.GetInfo());
+        Console.ReadKey();
+
+        ++roundNumber;
+      }
     }
   }
 }
+
+
