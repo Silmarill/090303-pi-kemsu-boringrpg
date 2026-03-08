3e1e8c6d5a47e7ab5e0e61446a9e4a8216c346da
skibidi-toilet-1337 <swqdhqh@gmail.com>
2026-03-08 18:31:55 +0700

    Что-то поменялось 3

--- Program.cs
+++ Program.cs
@@ -6,12 +6,11 @@ namespace BoringRPG {
       string critText;
       int beforeHP, damage;
 
-      //DummyClass skibidist = new DummyClass("Ланселот Ловкий");
       DummyClass artur = new DummyClass("Артур Пендрагон");
       Mage skibidist = new Mage("Скибидист Вапапапич");
 
-      Console.WriteLine($"НАЧАЛО БИТВЫ." + $"ПЕРВЫЙ РАУНД \n"+
-                        $" Исходное состояние: \n" +
+      Console.WriteLine($"НАЧАЛО БИТВЫ\n" + $"ПЕРВЫЙ РАУНД\n"+
+                        $"Исходное состояние: \n" +
                         $"==================\n" +
                         $"{skibidist.GetInfo()}\n" +
                         $"{artur.GetInfo()}\n");
@@ -31,14 +30,11 @@ namespace BoringRPG {
       Console.WriteLine("======================");
       Console.WriteLine(skibidist.GetInfo());
       Console.WriteLine(artur.GetInfo());
+      Console.ReadKey();
 
       /////////////////////////////////////////
-      ///
-      Console.WriteLine($"ВТОРОЙ РАУНД. Исходное состояние: \n" +
-                        $"==================\n" +
-                        $"{skibidist.GetInfo()}\n" +
-                        $"{artur.GetInfo()}\n");
 
+      Console.WriteLine($"\nВТОРОЙ РАУНД.");
 
       Console.WriteLine($"{artur.Name} атакует {skibidist.Name}!");
 
@@ -55,6 +51,29 @@ namespace BoringRPG {
       Console.WriteLine(skibidist.GetInfo());
       Console.WriteLine(artur.GetInfo());
       Console.ReadKey();
+
+      /////////////////////////////////////////////////////
+
+      Console.WriteLine($"\nТРЕТИЙ РАУНД.");
+
+      Console.WriteLine($"{skibidist.Name} атакует {artur.Name}!");
+
+      beforeHP = artur.HP;
+      skibidist.Hit(artur);
+      damage = beforeHP - artur.HP;
+
+      critText = artur.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+
+      Console.WriteLine($"Нанесено {damage} урона{critText}\n");
+
+      Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
+      Console.WriteLine("======================");
+      Console.WriteLine(skibidist.GetInfo());
+      Console.WriteLine(artur.GetInfo());
+      Console.ReadKey();
+
+
+
     }
   }
 }
