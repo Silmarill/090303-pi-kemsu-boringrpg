d4f979be60c559a966a9ced3a9b091fe9108212a
6lood1ess <6lo.od1ess@gmail.com>
2026-03-08 22:14:18 +0700

    The second and third rounds have been added to Program.cs

--- Program.cs
+++ Program.cs
@@ -5,13 +5,13 @@ namespace BoringRPG {
     static void Main(string[] args) {
       string critText;
       int beforeHP, damage;
-      int healing = 15;
+      int healing = 20;
       
       RogueClass lancelot = new RogueClass("Ланселот Ловкий");
       RogueClass rapfael = new RogueClass("Рафаэль Могучий");
       RogueClass optimus = new RogueClass("Оптимус Непрайм");
             
-      Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
+      Console.WriteLine($"НАЧАЛО БИТВЫ. ПЕРВЫЙ РАУНД\nИсходное состояние:\n" +
                         $"==================\n" +
                         $"{lancelot.GetInfo()}\n" +
                         $"{rapfael.GetInfo()}\n" +
@@ -44,8 +44,129 @@ namespace BoringRPG {
                         "======================\n" +
                         $"{lancelot.GetInfo()}\n" +
                         $"{rapfael.GetInfo()}\n" +
-                        $"{optimus.GetInfo()}");
+                        $"{optimus.GetInfo()}\n\n" +
+                        $"{optimus.Name} выбывает!\n");
+
+      Console.WriteLine("Нажмите любую клавишу, чтобы начать следующий раунд . . .");
       Console.ReadKey();
+
+      Console.WriteLine($"\nВТОРОЙ РАУНД\nИсходное состояние:\n" +
+                        $"==================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{rapfael.GetInfo()}\n");
+      
+      Console.WriteLine($"Кто-то выкрал все патроны у {rapfael.Name}: -{rapfael.Ammo} патронов! " + 
+                        "Осталось патронов: 0!");
+      rapfael.Ammo = 0;
+
+      Console.WriteLine($"{rapfael.Name} атакует {lancelot.Name}!");
+
+      beforeHP = lancelot.HP;
+      rapfael.Hit(lancelot);
+      damage = beforeHP - lancelot.HP;
+
+      critText = rapfael.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+
+      Console.WriteLine($"Нанесено {damage} урона{critText}\n");
+
+      if (lancelot) { 
+        Console.WriteLine($"{lancelot.Name} чудом выжил!\n");
+      } else {
+        Console.WriteLine($"{lancelot.Name} пал смертью храбрых!\n");
+
+        Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:\n" +
+                        "======================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{rapfael.GetInfo()}\n" +
+                        $"{lancelot.Name} выбывает!\n\n" +
+                        $"Поздравляем {rapfael.Name} с победой!\n");
+
+        return;
+      }
+
+      Console.WriteLine($"{lancelot.Name} использует прием \"Левый коронный, правый похоронный\"!");
+      beforeHP = rapfael.HP;
+      lancelot.Hit(rapfael);
+      lancelot.Hit(rapfael);
+      damage = beforeHP - rapfael.HP;
+
+      critText = lancelot.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+
+      Console.WriteLine($"Нанесено {damage} урона от спецприема!{critText}\n");
+
+      if (rapfael) { 
+        Console.WriteLine($"{rapfael.Name} чудом выжил!\n");
+      } else {
+        Console.WriteLine($"{rapfael.Name} пал смертью храбрых!\n");
+
+        Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:\n" +
+                        "======================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{rapfael.GetInfo()}\n" +
+                        $"{rapfael.Name} выбывает!\n\n" +
+                        $"Поздравляем {lancelot.Name} с победой!\n");
+
+        return;
+      }
+
+      Console.WriteLine("Нажмите любую клавишу, чтобы начать следующий раунд . . .");
+      Console.ReadKey();
+
+      Console.WriteLine($"\nТРЕТИЙ РАУНД\nИсходное состояние:\n" +
+                        $"==================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{rapfael.GetInfo()}\n");
+
+      Console.WriteLine($"{rapfael.Name} атакует {lancelot.Name}!");
+
+      beforeHP = lancelot.HP;
+      rapfael.Hit(lancelot);
+      damage = beforeHP - lancelot.HP;
+
+      critText = rapfael.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+
+      Console.WriteLine($"Нанесено {damage} урона{critText}\n");
+
+      if (lancelot) { 
+        Console.WriteLine($"{lancelot.Name} чудом выжил!\n");
+      } else {
+        Console.WriteLine($"{lancelot.Name} пал смертью храбрых!\n");
+
+        Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:\n" +
+                        "======================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{rapfael.GetInfo()}\n" +
+                        $"{lancelot.Name} выбывает!\n\n" +
+                        $"Поздравляем {rapfael.Name} с победой!\n");
+
+        return;
+      }
+
+      Console.WriteLine($"{lancelot.Name} атакует {rapfael.Name}!");
+      beforeHP = rapfael.HP;
+      lancelot.Hit(rapfael);
+      damage = beforeHP - rapfael.HP;
+
+      critText = lancelot.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+
+      Console.WriteLine($"Нанесено {damage} урона от спецприема!{critText}\n");
+
+      if (rapfael) { 
+        Console.WriteLine($"{rapfael.Name} чудом выжил!\n");
+      } else {
+        Console.WriteLine($"{rapfael.Name} пал смертью храбрых!\n");
+
+        Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:\n" +
+                        "======================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{rapfael.GetInfo()}\n" +
+                        $"{rapfael.Name} выбывает!\n\n" +
+                        $"Поздравляем {lancelot.Name} с победой!\n");
+
+        return;
+      }
+
+      Console.WriteLine("БОЙ ОКОНЧЕН НИЧЬЕЙ!");
     }
   }
 }
