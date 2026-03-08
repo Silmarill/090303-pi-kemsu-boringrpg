d18470a394a8b8e6ab457995d175e5e5d37c68fc
ksmushkov-cmd <ksmushkov@bk.ru>
2026-03-08 14:49:11 +0700

    Дополнение

--- Necromancer.cs
+++ Necromancer.cs
@@ -7,7 +7,10 @@ namespace BoringRPG {
     private static Random random = new Random();
     public bool LastHitWasCrit;
 
+    private int _skeletonBonus;
+
     public Necromancer(string name, int hp, int mp, int ammo, int dmg, double crit) : base("necromancer", 55, 90, 0, 30, 0.1) {
+      _skeletonBonus = 0;
     }
 
     public Necromancer(string name) : base("necromancer", 55, 90, 0, 30, 0.1) {
@@ -15,12 +18,12 @@ namespace BoringRPG {
 
     public static Necromancer operator +(Necromancer necromancer, int amount) {
       necromancer.HP += amount;
-      return amount;
+      return necromancer;
     }
 
     public static Necromancer operator -(Necromancer necromancer, int amount) {
       necromancer.HP -= amount;
-      return amount;
+      return necromancer;
     }
 
     public static bool operator true(Necromancer necromancer) {
@@ -32,20 +35,39 @@ namespace BoringRPG {
     }
       
     public override void Hit(Archetype target) {
-      int damage = Damage;
-      
-      // Метод NextDouble() - возвращает double в диапазоне [0.0; 1.0)
-      LastHitWasCrit = random.NextDouble() < CritChance;
-      
-      if (LastHitWasCrit) {
-        damage *= 2;
-      }
 
-      target.HP -= damage;
-    }
+      if (this.MP >= 15) { 
+
+        this.MP -= 15;
+        int damage = Damage + _skeletonBonus;
+
+        LastHitWasCrit = random.NextDouble() < CritChance;
+      
+        if (LastHitWasCrit) {
+          damage *= 2;
+          Console.WriteLine($"{Name}:Critical hit!");
+        }
+
+        int targetHpBefore = target.HP;
+                
+        
+        target.HP -= damage;
+                
+                
+        if (targetHpBefore > target.HP) {
+                    
+          _skeletonBonus += 5;
+          Console.WriteLine($"{Name}:Skeleton Summoned! Current Bonus:+{_skeletonBonus}");
+        }
+                
+        Console.WriteLine($"{Name} attacks {target.Name} and applies {damage} damage!");
+      } else {
+        Console.WriteLine($"{Name}:Not enough mana to attack! (15 MP needed)");
+      }
+    }  
 
     public override string GetInfo() {
-      return $"{Name} (Dummy): HP {HP}, MP {MP}, Ammo {Ammo}, Шанс крита {CritChance * 100}%";
+      return $"{Name} (necromancer): HP {HP}, MP {MP}, Ammo {Ammo}, Crit chance {CritChance * 100}%";
     }
   }
 }
