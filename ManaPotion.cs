7cc184803575332eb2b29111e4b675bb8113497d
Saibuu Vilena <vsaybuu@bk.ru>
2026-03-13 13:43:03 +0700

    ManaPotion добавлен

---
+++ ManaPotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG {
+   public class ManaPotion : ConsumableItem {
+    public ManaPotion(int value) : base(value) { }
+
+    public override string GetDescription() {
+      return $"Зелье маны (+{Value} MP)";
+    }
+  }
+}
+
