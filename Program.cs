bf99e22cc947da92bfb5d4149a5ac0fbcec55978
Saibuu Vilena <vsaybuu@bk.ru>
2026-03-13 11:51:26 +0700

    .

--- Program.cs
+++ Program.cs
@@ -1,37 +1,34 @@
 ﻿using System;
 
-namespace BoringRPG
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string critText;
-            int beforeHP, damage;
+namespace BoringRPG {
+  internal class Program {
+    static void Main(string[] args) {
+      string critText;
+      int beforeHP, damage;
 
-            Berserker berserker = new Berserker("Берсерк");
-            DummyClass target = new DummyClass("Цель");
+      Berserker berserker = new Berserker("Берсерк");
+      DummyClass target = new DummyClass("Цель");
 
-            Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
-                              $"==================\n" +
-                              $"{berserker.GetInfo()}\n" +
-                              $"{target.GetInfo()}\n");
+      Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
+                        $"==================\n" +
+                        $"{berserker.GetInfo()}\n" +
+                        $"{target.GetInfo()}\n");
 
-            Console.WriteLine($"{berserker.Name} атакует {target.Name}!");
+      Console.WriteLine($"{berserker.Name} атакует {target.Name}!");
 
-            beforeHP = target.HP;
-            berserker.Hit(target);
-            damage = beforeHP - target.HP;
+      beforeHP = target.HP;
+      berserker.Hit(target);
+      damage = beforeHP - target.HP;
 
-            critText = berserker.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
+      critText = berserker.LastHitWasCrit ? " - КРИТИЧЕСКИЙ УДАР!" : "";
 
-            Console.WriteLine($"Нанесено {damage} урона{critText}\n");
+      Console.WriteLine($"Нанесено {damage} урона{critText}\n");
 
-            Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
-            Console.WriteLine("======================");
-            Console.WriteLine(berserker.GetInfo());
-            Console.WriteLine(target.GetInfo());
-            Console.ReadKey();
-        }
+      Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
+      Console.WriteLine("======================");
+      Console.WriteLine(berserker.GetInfo());
+      Console.WriteLine(target.GetInfo());
+      Console.ReadKey();
     }
+  }
 }
