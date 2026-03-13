8711bc772e2716de96fcbcdb126827139f68ad76
Saibuu Vilena <vsaybuu@bk.ru>
2026-03-13 12:39:24 +0700

    ConsumableItem.cs добавлен

---
+++ ConsumableItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BoringRPG {
+  internal abstract class ConsumableItem {
+    public int Value { get; protected set; }
+
+    public ConsumableItem(int value) {
+      Value = value;
+    }
+
+    public abstract string GetDescription();
+  }
+}
