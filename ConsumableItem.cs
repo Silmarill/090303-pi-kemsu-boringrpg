c35709bd972d415b267aa919fbd27467539ff69a
Albertikk24 <albertikk24@gmail.com>
2026-03-13 14:48:19 +0700

    Добавил абстрактный класс ConsumableItem

---
+++ ConsumableItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BoringRPG {
+  public abstract class ConsumableItem {
+    public int Value { get; protected set; }
+    public string Name { get; protected set; }
+
+    protected ConsumableItem(string name, int value) {
+      Name = name;
+      Value = value;
+    }
+
+    public abstract string GetDescription();
+  }
+}
