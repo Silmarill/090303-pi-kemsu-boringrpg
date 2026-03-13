00c82c45e600a5844fef2e036b265b39e8ae3877
Kristina Shcherbakova <kristinagrom876@gmail.com>
2026-03-13 14:28:29 +0700

    change

--- ConsumableItem.cs
+++ ConsumableItem.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace BoringRPG {
+  public abstract class ConsumableItem {
+    public int Value;
+
+    public ConsumableItem(int value)
+    {
+      Value = value;
+    }
+  }
+}
--- HealthPotion.cs
+++ HealthPotion.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BoringRPG {
-  internal class HealthPotion {
+  public class HealthPotion : ConsumableItem {
+    public HealthPotion(int value) : base(value) { }
   }
 }
