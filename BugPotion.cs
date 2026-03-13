604ebf66d8325b527e274c15de807b8e6ad396c1
Saibuu Vilena <vsaybuu@bk.ru>
2026-03-13 13:43:31 +0700

    BugPotion добавлен

---
+++ BugPotion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG {
+  internal class BugPotion : ConsumableItem {
+    private static Random random = new Random();
+
+    public BugPotion() : base(0) {
+      Value = random.Next(1, 11);
+    }
+
+    public override string GetDescription() {
+      return $"Зелье-баг (случайный эффект: {Value})";
+    }
+  }
+}
