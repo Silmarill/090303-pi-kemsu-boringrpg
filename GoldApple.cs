4a14000e29e48979afcaa5bf034249f22ed3b910
6lood1ess <6lo.od1ess@gmail.com>
2026-03-13 14:47:52 +0700

    Remade CoffeeCup’s legacy class into GoldApple

---
+++ GoldApple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BoringRPG {
+  public class GoldApple : ConsumableItem {
+
+    public GoldApple (int value) : base (value) {
+    }
+
+    public override string GetEffectDescription() {
+      return $"Восстанавливает {Value} HP и увеличивает количество маны на {Value * 3}";
+    }
+  }
+}
