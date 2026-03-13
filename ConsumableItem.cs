236de4f5bd39c9174a110bbb17d2088aee15a714
Vova200804 <sanek_sanek_24@mail.ru>
2026-03-13 09:14:53 +0700

    (((

--- ConsumableItem.cs
+++ ConsumableItem.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using BoringRPG;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BoringRPG
 {
-  internal class ConsumableItem
+  public abstract class ConsumableItem
   {
+    public int Value;
+
+    public ConsumableItem(int value)
+    {
+      Value = value;
+    }
+    public abstract void Use(Nekromaster necromancer);
+  }
+}
+public class HealthPotion : ConsumableItem
+{
+  public HealthPotion(int value) : base(value)
+  { 
+    public override void Use(Nekromaster necromancer)
+    {
+    Nekromaster.hp += Value;
+    } 
   }
 }
