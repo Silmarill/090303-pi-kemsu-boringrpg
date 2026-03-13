f13bf253eda212eebe37519761196f00fd93b145
6lood1ess <6lo.od1ess@gmail.com>
2026-03-13 14:42:21 +0700

    Added AmmoPack legacy class

---
+++ AmmoPack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BoringRPG {
+  public class AmmoPack : ConsumableItem {
+
+    public AmmoPack(int value) : base(value) {
+    }
+
+    public override string GetEffectDescription() {
+      return $"Добавляет {Value} патронов";
+    }
+  }
+}
