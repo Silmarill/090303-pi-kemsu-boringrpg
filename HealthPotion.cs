e82e8e01a51f811a801c843d419070efc2d7362f
Saibuu Vilena <vsaybuu@bk.ru>
2026-03-13 13:20:49 +0700

    зелье здоровья добавлено

---
+++ HealthPotion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG {
+   public class HealthPotion : ConsumableItem {
+    public HealthPotion(int value) : base(value) { }
+
+    public override string GetDescription() {
+      return $"Зелье здоровья (+{Value} HP)";
+    }
+  }
+}
