ca6c8f14e6dea1f48f61d4767c1bc5efc6b69b7d
6lood1ess <6lo.od1ess@gmail.com>
2026-03-13 14:29:19 +0700

    Added ManaPotion legacy class

---
+++ ManaPotion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BoringRPG {
+  internal class ManaPotion : ConsumableItem {
+
+    public ManaPotion(int value) : base(value) {
+    }
+
+    public override string GetEffectDescription() {
+      return $"Восстанавливает {Value} MP";
+    }
+  }
+}
