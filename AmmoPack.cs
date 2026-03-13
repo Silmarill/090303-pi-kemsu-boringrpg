49f67bd46d4b7d622951509a0ae3a2e7c4998e5f
Saibuu Vilena <vsaybuu@bk.ru>
2026-03-13 13:41:00 +0700

    AmmoPack добавлен

---
+++ AmmoPack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG {
+   public class AmmoPack : ConsumableItem {
+    public AmmoPack(int value) : base(value) { }
+
+    public override string GetDescription() {
+      return $"Патроны (+{Value} Ammo)";
+    }
+  }
+}
+
