4622f10cbd00c5c5177d45e16aedd6b36c33d642
Egor12312 <uegor6705@gmail.com>
2026-03-14 22:49:21 +0700

    код

---
+++ DoubleEspresso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BoringRPG {
+  public class DoubleEspresso : ConsumableItem {
+    public DoubleEspresso(int value) : base(value) { }
+
+    public static DoubleEspresso operator *(DoubleEspresso coffee, int multiplier)
+    {
+      return new DoubleEspresso(coffee.Value * multiplier);
+    }
+
+    public static Druid operator >(Druid druid, DoubleEspresso coffee)
+    {
+      druid.Damage += coffee.Value;
+      druid.HP -= coffee.Value / 2;
+      druid.MP += coffee.Value / 2;
+      Console.WriteLine($" {druid.Name} выпил двойной эспрессо! Урон +{coffee.Value}, " +
+                       $"здоровье -{coffee.Value / 2}, мана +{coffee.Value / 2}");
+      return druid;
+    }
+
+    public static Druid operator <(Druid druid, DoubleEspresso coffee)
+    {
+      return druid;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,53 +1,57 @@
 ﻿using System;
 
 namespace BoringRPG {
-  public class Program {
+  internal class Program {
     static void Main(string[] args)
     {
       Console.WriteLine("=== ДЕМОНСТРАЦИЯ РАСХОДУЕМЫХ ПРЕДМЕТОВ ===\n");
 
       Druid cedric = new Druid("Кедрик Зелёный");
 
-      Console.WriteLine($"Начальное состояние Кедрика:");
+      Console.WriteLine("Начальное состояние:");
       Console.WriteLine(cedric.GetInfo());
       Console.WriteLine();
 
-      Console.WriteLine("1. Используем обычные предметы:");
+      Console.WriteLine("1. Использование обычных предметов:");
+      Console.WriteLine("-------------------------------------");
 
       HealthPotion healthPotion = new HealthPotion(30);
       cedric += healthPotion;
-      Console.WriteLine($"После HealthPotion(+30 HP): {cedric.GetInfo()}");
+      Console.WriteLine(cedric.GetInfo());
 
       ManaPotion manaPotion = new ManaPotion(20);
       cedric += manaPotion;
-      Console.WriteLine($"После ManaPotion(+20 MP): {cedric.GetInfo()}");
+      Console.WriteLine(cedric.GetInfo());
 
       AmmoPack ammoPack = new AmmoPack(15);
       cedric += ammoPack;
-      Console.WriteLine($"После AmmoPack(+15 Ammo): {cedric.GetInfo()}");
-
-      Console.WriteLine("\n=== Демонстрация боя с предметами ===");
-      Druid morgan = new Druid("Морган Лесная");
-
-      Console.WriteLine($"Исходное состояние Морган:");
-      Console.WriteLine(morgan.GetInfo());
-
-      morgan += new HealthPotion(25);
-      Console.WriteLine($"\nМорган выпила зелье здоровья (+25 HP):");
-      Console.WriteLine(morgan.GetInfo());
-
-      Console.WriteLine($"\n{morgan.Name} атакует {cedric.Name}!");
-      int beforeHP = cedric.HP;
-      morgan.Hit(cedric);
-      int damage = beforeHP - cedric.HP;
-
-      Console.WriteLine($"Нанесено {damage} урона");
-      Console.WriteLine($"После атаки Кедрик: {cedric.GetInfo()}");
-
-      cedric += new HealthPotion(20);
-      Console.WriteLine($"\nКедрик использовал аптечку после боя:");
       Console.WriteLine(cedric.GetInfo());
 
+      Console.WriteLine();
+
+      Console.WriteLine("2. Использование безумных предметов:");
+      Console.WriteLine("-------------------------------------");
+
+      DoubleEspresso espresso = new DoubleEspresso(10);
+
+      if (cedric > espresso)
+      {
+      }
+      Console.WriteLine(cedric.GetInfo());
+
+      Console.WriteLine();
+
+      Console.WriteLine("3. Пробуем двойной эспрессо с оператором >:");
+      Console.WriteLine("-------------------------------------");
+
+      DoubleEspresso coffee = new DoubleEspresso(20);
+
+      if (cedric > coffee)
+      {
+      }
+      Console.WriteLine(cedric.GetInfo());
+
+      Console.WriteLine("\nНажмите любую клавишу для выхода...");
       Console.ReadKey();
     }
   }
