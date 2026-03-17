5847382def1ebbda9a150879caa3108c18ec1791
Asverktus <aleksandr.rubin123@gmail.com>
2026-03-17 20:38:24 +0700

    Ну вроде расходники готовы

---
+++ ConsumableItem.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace BoringRPG
+{
+  internal abstract class ConsumableItem
+  {
+    public int Value { get; protected set; }
+
+    protected ConsumableItem(int value)
+    {
+      Value = value;
+    }
+
+    public abstract void ApplyEffect(Archetype target);
+  }
+
+  internal class HealthPotion : ConsumableItem
+  {
+    public HealthPotion(int value) : base(value) { }
+
+    public override void ApplyEffect(Archetype target)
+    {
+      target.HP += Value;
+      Console.WriteLine($"{target.Name} восстановил {Value} HP!");
+    }
+  }
+
+  internal class ManaPotion : ConsumableItem
+  {
+    public ManaPotion(int value) : base(value) 
+    { 
+    }
+
+    public override void ApplyEffect(Archetype target)
+    {
+      target.MP += Value;
+      Console.WriteLine($"{target.Name} восстановил {Value} MP!");
+    }
+  }
+
+  internal class AmmoPack : ConsumableItem
+  {
+    public AmmoPack(int value) : base(value) { }
+
+    public override void ApplyEffect(Archetype target)
+    {
+      target.Ammo += Value;
+      Console.WriteLine($"{target.Name} получил {Value} патронов!");
+    }
+  }
+
+  internal class EnergyDrink : ConsumableItem
+  {
+    public EnergyDrink(int value) : base(value) { }
+
+    public override void ApplyEffect(Archetype target)
+    {
+      target.Damage += Value / 2;
+      target.HP -= Value / 3;
+
+      Console.WriteLine($"⚡ {target.Name} выпил энергетик! Урон увеличен на {Value / 2}, но потеряно {Value / 3} HP!");
+    }
+
+    public static EnergyDrink operator ++(EnergyDrink drink)
+    {
+      drink.Value *= 2;
+      Console.WriteLine($"☢️ Энергетик стал еще крепче! Теперь его величина {drink.Value}");
+      return drink;
+    }
+  }
+
+  internal static class ConsumableHelper
+  {
+    public static void UseItem(this Archetype character, ConsumableItem item)
+    {
+      item.ApplyEffect(character);
+    }
+  }
+
+  internal class CharacterWithItems
+  {
+    public Archetype Character { get; set; }
+
+    public CharacterWithItems(Archetype character)
+    {
+      Character = character;
+    }
+
+    public static CharacterWithItems operator +(CharacterWithItems wrapper, ConsumableItem item)
+    {
+      item.ApplyEffect(wrapper.Character);
+      return wrapper;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 
-namespace BoringRPG {
-  internal class Program {
-    static void Main(string[] args) {
+namespace BoringRPG
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
       string critText;
       int beforeHP, damage;
-      
+
       DummyClass lancelot = new DummyClass("Ланселот Ловкий");
       HunterClass amogus = new HunterClass("Амогус Гус");
 
       int healPackage;
       healPackage = 15;
+
+      Console.WriteLine("\n----- ИСПОЛЬЗОВАНИЕ ПРЕДМЕТОВ -----");
+
+      HealthPotion healthPotion = new HealthPotion(30);
+      ManaPotion manaPotion = new ManaPotion(20);
+      AmmoPack ammoPack = new AmmoPack(8);
+      EnergyDrink energyDrink = new EnergyDrink(24);
+
+      CharacterWithItems wrappedLancelot = new CharacterWithItems(lancelot);
+      CharacterWithItems wrappedAmogus = new CharacterWithItems(amogus);
+
+      Console.WriteLine($"\n{amogus.Name} находит зелье здоровья ({healthPotion.Value} ед.)!");
+      wrappedAmogus = wrappedAmogus + healthPotion;
+
+      Console.WriteLine($"\n{lancelot.Name} находит зелье маны ({manaPotion.Value} ед.)!");
+      wrappedLancelot = wrappedLancelot + manaPotion;
+
+      Console.WriteLine($"\n{amogus.Name} находит патроны ({ammoPack.Value} шт.)!");
+      wrappedAmogus = wrappedAmogus + ammoPack;
+
+      Console.WriteLine($"\n{amogus.Name} выпивает энергетик!");
+      wrappedAmogus = wrappedAmogus + energyDrink;
+
+      Console.WriteLine($"\n{amogus.Name} находит второй энергетик и смешивает их!");
+      ++energyDrink;
+      wrappedAmogus = wrappedAmogus + energyDrink;
+
+      Console.WriteLine("\n----- СОСТОЯНИЕ ПОСЛЕ ПРЕДМЕТОВ -----");
+      Console.WriteLine(lancelot.GetInfo());
+      Console.WriteLine(amogus.GetInfo());
+      Console.WriteLine("\n----- ПРОДОЛЖЕНИЕ БИТВЫ -----");
 
       Console.WriteLine("Амогус находит аптечку и лечится!");
       amogus = amogus + healPackage;
@@ -22,10 +56,10 @@ namespace BoringRPG {
       }
 
       Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
-                              $"==================\n" +
-                              $"{lancelot.GetInfo()}\n" +
-                              $"{amogus.GetInfo()}\n");
-           
+                                $"==================\n" +
+                                $"{lancelot.GetInfo()}\n" +
+                                $"{amogus.GetInfo()}\n");
+
       Console.WriteLine($"{amogus.Name} атакует {lancelot.Name}");
 
       beforeHP = lancelot.HP;
