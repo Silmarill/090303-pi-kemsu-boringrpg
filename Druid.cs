7bbad32d51d597f1edb8eab36e8b4047759797c1
neldori <nexusstail@gmail.com>
2026-03-17 15:51:16 +0700

    test2

--- Druid.cs
+++ Druid.cs
@@ -2,6 +2,11 @@
 
 namespace BoringRPG {
   internal class Druid : Archetype {
+    private int maxHealth = 90;
+    private int maxMana = 60;
+    private int manaCost = 5; 
+    private int hpThreshold = 45; 
+
     private static Random random = new Random();
     public bool LastHitWasCrit;
 
@@ -9,14 +14,36 @@ namespace BoringRPG {
         : base(name, 90, 60, 0, 20, 0.10) {
     }
 
-    public static Druid operator +(Druid druid, int regain) {
-      int maxHP = 90; 
+    public static bool operator true(Druid druid) {
+      return druid.HP > 0;
+    }
 
-      if (druid.HP < maxHP) {
+    public static bool operator false(Druid druid) {
+      return druid.HP <= 0;
+    }
+
+    public static Druid operator +(Druid druid, int regain) {
+      if (druid.HP < druid.maxHealth) {
         druid.HP = druid.HP + regain;
-        if (druid.HP > maxHP) {
-          druid.HP = maxHP;
+        if (druid.HP > druid.maxHealth) {
+          druid.HP = druid.maxHealth;
         }
+      }
+      return druid;
+    }
+
+    public static Druid operator +(Druid druid, HealthPotion healthPotion) {
+      druid.HP += healthPotion.Value;
+      if (druid.HP > druid.maxHealth) {
+        druid.HP = druid.maxHealth;
+      }
+      return druid;
+    }
+
+    public static Druid operator +(Druid druid, ManaPotion manaPotion) {
+      druid.MP += manaPotion.Value;
+      if (druid.MP > druid.maxMana) {
+        druid.MP = druid.maxMana;
       }
       return druid;
     }
@@ -34,28 +61,36 @@ namespace BoringRPG {
     }
 
     public override void Hit(Archetype target) {
-      int manaCost = 5; 
-      int baseDamage = this.Damage;
-      int hpThreshold = 45; 
+      int minMana = 0;
+      int minHp = 0;
+
+      int damage = this.Damage;
 
       if (this.MP >= manaCost) {
-        // Тратим ману
         this.MP -= manaCost;
-
-        int damage = baseDamage;
 
         if (target.HP > hpThreshold) {
           damage *= 2;
+          LastHitWasCrit = true; 
+        }
+        else {
+          LastHitWasCrit = false;
         }
 
         target.HP -= damage;
 
+        if (target.HP < minHp) {
+          target.HP = minHp;
+        }
+      }
+      else {
         LastHitWasCrit = false;
+        Console.WriteLine($"{Name} (Druid) Not enough mana to attack!");
       }
     }
 
     public override string GetInfo() {
-      return $"({Name}) Druid: HP {HP}/{90}, MP {MP}/{60}, Ammo {Ammo}, Damage {Damage}, Crit Chance: {CritChance * 100}%";
+      return $"{Name} (Druid): HP {HP}/{maxHealth}, MP {MP}/{maxMana}, Damage {Damage}, Crit Chance: {CritChance * 100}%";
     }
   }
 }
