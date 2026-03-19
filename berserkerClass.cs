94db784f673d2fc017013cc9794ba39951fe70c1
wacper <wacpercs@gmail.com>
2026-03-20 00:36:11 +0700

    completed(?)

--- berserkerClass.cs
+++ berserkerClass.cs
@@ -1,32 +1,40 @@
-﻿using System;
+using System;
 
 namespace BoringRPG {
   internal class BerserkerClass : Archetype {
+
     private static Random random = new Random();
-    private bool LastHitWasCrit;
+    private int maxHP;
+    public bool LastHitWasCrit;
 
-    public BerserkerClass(string name, int hp, int mp, int ammo, int dmg, double crit) : base(name, 140, 0, 0, 30, 0.15) {
-     }
+    public BerserkerClass(string name, int hp, int mp, int ammo, int dmg, double crit) : base(name, hp, mp, ammo, dmg, crit) {
+      maxHP = hp;
+    }
 
-    public BerserkerClass(string name) : base(name, 100, 50, 10, 20, 0.3) {
-     }
+    public BerserkerClass(string name) : base(name, 140, 0, 0, 30, 0.15) {
+      // HP=140, MP=0, Ammo=0, Damage=30, CritChance=0.15 (15%)
+      maxHP = 140;
+    }
+
     public override void Hit(Archetype target) {
-      int damage = Damage;
-      
+      int rageBonus = (maxHP - HP) / 2;
+      int damage = Damage + rageBonus;
+
       LastHitWasCrit = random.NextDouble() < CritChance;
-      
+
       if (LastHitWasCrit) {
         damage *= 2;
       }
 
-      if (LastHitWasCrit) {
-        HP -= damage;
-      }
-
       target.HP -= damage;
     }
+
     public override string GetInfo() {
-      return $"{Name} (Dummy): HP {HP}, MP {MP}, Ammo {Ammo}, Шанс крита {CritChance * 100}%";
+      int rageBonus = (maxHP - HP) / 2;
+      return Name + " (Berserker): HP " + HP + "/" + maxHP +
+             ", MP " + MP + ", Ammo " + Ammo +
+             ", Damage " + Damage + " (+" + rageBonus + " rage)" +
+             ", Шанс крита " + (CritChance * 100) + "%";
     }
-   }
+  }
 }
