eba6418d0278e320a06a15386080704b844dc7aa
v01d <Kevin_Vearez@mail.ru>
2026-03-19 23:39:26 +0700

    add abstract ConsumableItem base class

---
+++ ConsumableItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG {
+  internal abstract class ConsumableItem {
+    public int Value { get; set; }
+
+    protected ConsumableItem(int value) {
+      Value = value;
+    }
+
+    // A method that will modify the characteristics of a specific hero
+    public abstract void Apply(Archetype hero);
+  }
+}
