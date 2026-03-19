f9792aab45819769c6a854df1abf0865df3eb2db
v01d <Kevin_Vearez@mail.ru>
2026-03-19 23:40:58 +0700

    implement Health, Mana and Ammo consumables

---
+++ StandardItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG {
+  // Heal
+  internal class HealthPotion : ConsumableItem {
+    public HealthPotion(int value) : base(value) { }
+    public override void Apply(Archetype hero) {
+      hero.HP += Value;
+    }
+  }
+
+  // Mana
+  internal class ManaPotion : ConsumableItem {
+    public ManaPotion(int value) : base(value) { }
+    public override void Apply(Archetype hero) {
+      hero.MP += Value;
+    }
+  }
+
+  // Ammo
+  internal class AmmoPack : ConsumableItem {
+    public AmmoPack(int value) : base(value) { }
+    public override void Apply(Archetype hero) {
+      hero.Ammo += Value;
+    }
+  }
+}
