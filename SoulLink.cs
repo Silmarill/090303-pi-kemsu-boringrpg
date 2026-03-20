2f32912902d182570fe98c2bf25ec509dbe2bcea
Vova200804 <sanek_sanek_24@mail.ru>
2026-03-20 08:44:08 +0700

    Добавил Soulink

---
+++ SoulLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BoringRPG
+{
+  class SoulLink : Skill
+  {
+    public override void Use(Archetype dungeonMaster, Archetype artur)
+    {
+      int totalHP = dungeonMaster.HP + artur.HP;
+      dungeonMaster.HP = totalHP / 2;
+      artur.HP = totalHP / 2;
+    }
+  }
+}
