e97d7f7757076aa2765075bcb853123a0fcf9cc8
6lood1ess <6lo.od1ess@gmail.com>
2026-03-20 14:49:29 +0700

    - Completed the DramaAction child class

--- DramaAction.cs
+++ DramaAction.cs
@@ -3,6 +3,9 @@
 namespace BoringRPG {
   internal class DramaAction : Skill {
     static Random random = new Random();
+    int chance;
+    int firstChancePoint = 30;
+    int secondChancePoint = 60;
 
     public DramaAction() : base("DramaAction") { 
     }
@@ -10,7 +13,21 @@ namespace BoringRPG {
     public overrise void Use(Archetype caster, Archetype target) {
       Console.WriteLine($"{caster.Name} использует навык {Name} на {target.Name}");
 
-      //В процессе
+      chance = random.Next(100);
+
+      if (chance < firstChancePoint) { 
+        string oldName = target.Name;
+        target.Name = "Побежденный" + oldName;
+        Console.WriteLine($"{oldName} переименован в {target.Name}");
+      }
+
+      if (chance < secondChancePoint) { 
+        string oldName = caster.Name;
+        caster.Name = "Легендарный" + oldName;
+        Console.WriteLine($"{oldName} переименован в {caster.Name}");
+      } else { 
+        Console.WriteLine($"{caster.Name} дарит розу {target.Name}");
+      }
     }
   }
 }
