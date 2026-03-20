335e3362d92c8cab32b93e8f634a308f8998f97a
wacper <wacpercs@gmail.com>
2026-03-20 09:30:26 +0700

    Filled out the file.

---
+++ MP.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BoringRPG {
+  internal class ManaPotion : ConsumableItem {
+    public ManaPotion(int value) : base(value) { }
+
+    public override void Apply(Archetype target)
+    {
+      target.MP += Value;
+    }
+    }
+}
