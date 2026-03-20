b172abc9eff7a84bafe5f55550e695d0902bb067
RudDavid <satorugodze17@gmail.com>
2026-03-20 08:26:52 +0700

    Начало 3 этапа

---
+++ Skills/ICanUseSkill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG.Skills
+{
+    interface ICanUseSkill
+    {
+        void UseSkill(Skill skill, Archetype target);
+    }
+}
