88e3bcb970476fb532aad41d4ac833f5d5e06dd9
6lood1ess <6lo.od1ess@gmail.com>
2026-03-20 14:36:06 +0700

    - Created a DramaAction child class

---
+++ DramaAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BoringRPG {
+  internal class DramaAction : Skill {
+    static Random random = new Random();
+
+    public DramaAction() : base("DramaAction") { 
+    }
+
+    public overrise void Use(Archetype caster, Archetype target) {
+      Console.WriteLine($"{caster.Name} использует навык {Name} на {target.Name}");
+
+      //В процессе
+    }
+  }
+}
