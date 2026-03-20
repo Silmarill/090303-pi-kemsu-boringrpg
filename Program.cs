978058594164175ee7d06eec034a77b277006820
wacper <wacpercs@gmail.com>
2026-03-20 08:26:10 +0700

    Changed WriteLines

--- Program.cs
+++ Program.cs
@@ -11,10 +11,10 @@ namespace BoringRPG {
       BerserkerClass berserk = new BerserkerClass("Berserker");
 
       Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
-                               $"==================\n" +
-                               $"{lancelot.GetInfo()}\n" +
-                               $"{artur.GetInfo()}\n" +
-                               $"{berserk.GetInfo()}\n");
+                        $"==================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{artur.GetInfo()}\n" +
+                        $"{berserk.GetInfo()}\n");
 
       Console.WriteLine($"{lancelot.Name} атакует {artur.Name}!");
       Console.WriteLine($"{berserk.Name} атакует {lancelot.Name}");
@@ -31,19 +31,19 @@ namespace BoringRPG {
 
       Console.WriteLine("Нанесено " + damage + " урона" + critText + "\n");
 
-      Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
-      Console.WriteLine("======================");
-      Console.WriteLine(lancelot.GetInfo());
-      Console.WriteLine(artur.GetInfo());
-      Console.WriteLine("======================");
-      Console.WriteLine(lancelot.GetInfo());
-      Console.WriteLine(berserk.GetInfo());
+      Console.WriteLine($"ИТОГОВОЕ СОСТОЯНИЕ: \n" +
+                        $"======================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{artur.GetInfo()}\n" +
+                        $"======================\n" +
+                        $"{lancelot.GetInfo()}\n" +
+                        $"{berserk.GetInfo()}\n");
 
       // Этап 2: Демонстрация расходников
 
-      Console.WriteLine("\n==========================================");
-      Console.WriteLine("ПРИВАЛ. Время расходников.");
-      Console.WriteLine("==========================================\n");
+      Console.WriteLine($"\n==========================================\n" +
+                        $"ПРИВАЛ. Время расходников.\n" +
+                        $"==========================================\n");
 
       Archetype target;
 
@@ -65,12 +65,12 @@ namespace BoringRPG {
       Console.WriteLine("До: " + lancelot.GetInfo());
       target = lancelot;
       target *= new CoffeeCup(3);
-      Console.WriteLine("После CoffeeCup(3): " + lancelot.GetInfo());
-      Console.WriteLine("(тройной эспрессо. Руки дрожат, но крит-шанс утроен.)");
+      Console.WriteLine($"После CoffeeCup(3): " + lancelot.GetInfo());
+      Console.WriteLine($"(тройной эспрессо. Руки дрожат, но крит-шанс утроен.)");
 
 
       interface ICanUseSkill {
-        void UseSkill()
+            void UseSkill(Skill skill, Archetype target);
       }
       Console.ReadKey();
     }
