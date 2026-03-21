9edf743a5aa8de65155648400107c2a3c787e417
Konstantin <H1dra5@yandex.ru>
2026-03-21 17:15:38 +0700

    Реализовал в program SoulLink

--- Program.cs
+++ Program.cs
@@ -9,9 +9,10 @@ namespace BoringRPG {
       Heal hPotion = new Heal(42);
       ManaRestore mPotion = new ManaRestore(42);
       AmmoPack aPotion = new AmmoPack(5);
-      FatBurger burger = new FatBurger(100);
+      FatBurger burger = new FatBurger(50);
       Rogue torfin = new Rogue("Торфин Безопасный");
       DummyClass killer = new DummyClass("Скрытный убийца");
+      Skill soulLink = new SoulLink();
 
       Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
                         $"{torfin.GetInfo()}\n" + $"{killer.GetInfo()}\n");
@@ -30,10 +31,14 @@ namespace BoringRPG {
 
       Console.WriteLine($"\n{torfin.Name} После хила решил перекусить и потолстел на -{burger.value}, но при этом увеличил свой урон {burger.value}");
       torfin -= burger;
+
+      Console.WriteLine($"Чтобы не проиграть, Торфин использует способность SoulLink и уравнивает своё хп с хп противника\n");
+      torfin.UseSkill(soulLink, killer);
                        
       Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
       Console.WriteLine("======================");
       Console.WriteLine(torfin.GetInfo());
+      Console.WriteLine(killer.GetInfo());
       Console.ReadKey();
     }
   }
