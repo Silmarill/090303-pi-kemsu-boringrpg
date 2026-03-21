94e812ec8d785a934b4676a966a492e8a3c9d626
Konstantin <H1dra5@yandex.ru>
2026-03-21 16:05:53 +0700

    Добавил способность SoulLink

--- SoulLink.cs
+++ SoulLink.cs
@@ -5,8 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BoringRPG { 
-  public SoulLink : Skill {
-  
-  public override void Use(Archetype)
+  class SoulLink : Skill {
+    int soulHP;
+    public override void Use(Archetype caster, Archetype target) {
+      Console.WriteLine($"{caster.Name} Использует SoulLink на {target.Name}");
+      soulHP = (caster.HP + target.HP) / 2;
+      caster.HP = soulHP;
+      target.HP = soulHP;
+    }
   }
 }
