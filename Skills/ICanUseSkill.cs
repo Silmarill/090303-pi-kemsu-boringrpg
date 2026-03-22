d33d79f9d9ee31a81838f83a184d2a80f4c29bf3
v01d <Kevin_Vearez@mail.ru>
2026-03-22 16:54:36 +0700

    Modified arch. , made interface and abstr. class
    The project architecture has been modified. An interface has been created that enables heroes to use skills, along with an abstract class for all future abilities.

---
+++ Skills/ICanUseSkill.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// An interface, that will allow heroes to use skills
+namespace BoringRPG {
+  internal interface ICanUseSkill {
+    void UseSkill(Skill skill, Archetype target);
+  }
+}
---
+++ Skills/Skill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Abstract class for all future abilities
+namespace BoringRPG {
+  internal abstract class Skill {
+    public string Name { get; protected set; }
+    public abstract void Use(Archetype caster, Archetype target);
+  }
+}
