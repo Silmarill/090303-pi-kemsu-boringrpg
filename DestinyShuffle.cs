a6490557863b1080a0ba7ba91be8d747d6863146
DaniruAnime <animeshnikdaniru@gmail.com>
2026-03-24 00:24:01 +0700

    начало реализации DestinyShuffle

---
+++ DestinyShuffle.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace BoringRPG
+{
+    internal class DestinyShuffle : Skill
+    {
+        public DestinyShuffle(string name, int mana) : base(name, mana)
+        {
+        }
+
+        public override void Use(Archetype caster, Archetype target)
+        {
+            
+            
+        }
+
+    }
+}
