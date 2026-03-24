5775baf6f416c8b384dbe4a94b817326993fe97f
DaniruAnime <animeshnikdaniru@gmail.com>
2026-03-24 18:11:35 +0700

    починил рандом

--- Skill/DestinyShuffle.cs
+++ Skill/DestinyShuffle.cs
@@ -13,36 +13,42 @@ namespace BoringRPG
 
         public override void Use(Archetype caster, Archetype target)
         {
-            double[] parametersCaster = { caster.Ammo, caster.CritChance, caster.Damage, caster.HP, caster.MP };
-            double[] parametersTarget = { target.Ammo, target.CritChance, target.Damage, target.HP, target.MP };
+            double[] statsCaster = { caster.Ammo, caster.CritChance, caster.Damage, caster.HP, caster.MP };
+            double[] statsTarget = { target.Ammo, target.CritChance, target.Damage, target.HP, target.MP };
 
-            double[] randomParamCaster = randomParameters.GetItems(parametersCaster, 3);
+            /*double[] randomParamCaster = randomParameters.GetItems(parametersCaster, 3);
             double[] randomParamTarget = randomParameters.GetItems(parametersTarget, 3);
 
-            int randomRND;
+            int randomRND;*/
+
+            int indexCaster;
+            int indexTarget;
+            double temp;
 
             for (int randomIteration = 0; randomIteration < 3; ++randomIteration)
             {
-                randomRND = randomParameters.Next(0, 3);
+                indexCaster = randomParameters.Next(0, 5);
+                indexTarget = randomParameters.Next(0, 5);
 
-                parametersCaster[randomRND] = randomParamTarget[randomRND];
-                parametersTarget[randomRND] = randomParamCaster[randomRND];
+                temp = statsCaster[indexCaster];
+                statsCaster[indexCaster] = statsTarget[indexTarget];
+                statsTarget[indexTarget] = temp;
+
+                /*parametersCaster[randomRND] = randomParamTarget[randomRND];
+                parametersTarget[randomRND] = randomParamCaster[randomRND];*/
             }
 
-            Console.WriteLine(string.Join(", ", parametersCaster));
-            Console.WriteLine(string.Join(", ", parametersTarget));
+            caster.Ammo = (int)statsCaster[0];
+            caster.CritChance = statsCaster[1];
+            caster.Damage = (int)statsCaster[2];
+            caster.HP = (int)statsCaster[3];
+            caster.MP = (int)statsCaster[4];
 
-            caster.Ammo = (int)parametersCaster[0];
-            caster.CritChance = parametersCaster[1];
-            caster.Damage = (int)parametersCaster[2];
-            caster.HP = (int)parametersCaster[3];
-            caster.MP = (int)parametersCaster[4];
-
-            target.Ammo = (int)parametersTarget[0];
-            target.CritChance = parametersTarget[1];
-            target.Damage = (int)parametersTarget[2];
-            target.HP = (int)parametersTarget[3];
-            target.MP = (int)parametersTarget[4];
+            target.Ammo = (int)statsTarget[0];
+            target.CritChance = statsTarget[1];
+            target.Damage = (int)statsTarget[2];
+            target.HP = (int)statsTarget[3];
+            target.MP = (int)statsTarget[4];
 
         }
     }
