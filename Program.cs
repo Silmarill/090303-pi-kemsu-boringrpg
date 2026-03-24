7d8bb8825424374f173316a57ba83bcdbf7028af
neldori <nexusstail@gmail.com>
2026-03-24 20:17:49 +0700

    изменен

--- Program.cs
+++ Program.cs
@@ -3,79 +3,89 @@
 namespace BoringRPG {
   internal class Program {
     static void Main(string[] args) {
-      string critText;
       int beforeHP, damage;
-      int beforeMP, beforeDamage;
+      int beforeMP;
 
-      DummyClass lancelot = new DummyClass("Рагнар Простой");
-      Druid merlin = new Druid("Мерлин Друид");
+      // Создаем двух друидов
+      Druid merlin = new Druid("Мерлин");
+      Druid morrigan = new Druid("Морриган");
 
-      Console.WriteLine($"НАЧАЛО БИТВЫ. Исходное состояние: \n" +
+      Console.WriteLine($"НАЧАЛО. Исходное состояние: \n" +
       $"======================\n" +
-      $"{lancelot.GetInfo()}\n" +
-      $"{merlin.GetInfo()}\n");
+      $"{merlin.GetInfo()}\n" +
+      $"{morrigan.GetInfo()}\n");
 
-      Console.WriteLine($"\n=== ДЕМОНСТРАЦИЯ ПРЕДМЕТОВ ДЛЯ ДРУИДА ===\n");
+      // ДЕМОНСТРАЦИЯ ПРЕДМЕТОВ
+      Console.WriteLine($"\n=== ДЕМОНСТРАЦИЯ ПРЕДМЕТОВ ===\n");
 
       Console.WriteLine($"{merlin.Name} использует зелье здоровья.");
       HealthPotion healthPotion = new HealthPotion(15);
       beforeHP = merlin.HP;
       merlin += healthPotion;
-      Console.WriteLine($"{merlin.Name} восстановил {merlin.HP - beforeHP} HP\n");
+      Console.WriteLine();
 
       Console.WriteLine($"{merlin.Name} использует зелье маны.");
       ManaPotion manaPotion = new ManaPotion(20);
       beforeMP = merlin.MP;
       merlin += manaPotion;
-      Console.WriteLine($"{merlin.Name} восстановил {merlin.MP - beforeMP} MP\n");
+      Console.WriteLine();
 
       Console.WriteLine($"{merlin.Name} использует зелье природы.");
       NaturePotion naturePotion = new NaturePotion(5);
-      beforeDamage = merlin.Damage;
       merlin += naturePotion;
-      Console.WriteLine($"{merlin.Name} увеличил урон на {merlin.Damage + 5 - beforeDamage}\n");
+      Console.WriteLine();
 
       Console.WriteLine($"{merlin.Name} съедает лунную ягоду.");
-      MoonBerry berry = new MoonBerry(15); 
+      MoonBerry berry = new MoonBerry(15);
       merlin += berry;
       Console.WriteLine();
 
-      Console.WriteLine($"Состояние после использования всех предметов:");
+      Console.WriteLine($"Состояние после предметов:");
       Console.WriteLine(merlin.GetInfo());
       Console.WriteLine();
 
+      // ДЕМОНСТРАЦИЯ НАВЫКОВ
+      Console.WriteLine($"=== ДЕМОНСТРАЦИЯ НАВЫКОВ ===\n");
+
+      // Навык 1: SoulLink
+      Skill soulLink = new SoulLink();
+      merlin.UseSkill(soulLink, morrigan);
+      Console.WriteLine($"После SoulLink:");
+      Console.WriteLine(morrigan.GetInfo());
+      Console.WriteLine(merlin.GetInfo());
+      Console.WriteLine();
+
+      // Навык 2: ManaDrain
+      Skill manaDrain = new ManaDrain();
+      merlin.UseSkill(manaDrain, morrigan);
+      Console.WriteLine($"После ManaDrain:");
+      Console.WriteLine(morrigan.GetInfo());
+      Console.WriteLine(merlin.GetInfo());
+      Console.WriteLine();
+
+      // БИТВА
       Console.WriteLine($"=== БИТВА ===\n");
-      Console.WriteLine($"{merlin.Name} атакует {lancelot.Name}!");
+      Console.WriteLine($"{merlin.Name} атакует {morrigan.Name}!");
 
-      beforeHP = lancelot.HP;
-      merlin.Hit(lancelot);
-      damage = beforeHP - lancelot.HP;
+      beforeHP = morrigan.HP;
+      merlin.Hit(morrigan);
+      damage = beforeHP - morrigan.HP;
 
-      critText = merlin.LastHitWasCrit ? " – КРИТИЧЕСКИЙ УДАР!" : "";
-
+      string critText = merlin.LastHitWasCrit ? " – КРИТИЧЕСКИЙ УДАР!" : "";
       Console.WriteLine($"Нанесено {damage} урона{critText}\n");
 
-      Console.WriteLine($"=== ДЕМОНСТРАЦИЯ БЕЗУМНОГО ОПЕРАТОРА * ===\n");
-      Console.WriteLine($"{merlin.Name} находит ещё две лунные ягоды и съедает их!");
-
-      MoonBerry secondBerry = new MoonBerry(0);
-      merlin *= secondBerry; 
+      // Навык 3: DramaAction
+      Skill drama = new DramaAction();
+      morrigan.UseSkill(drama, merlin);
+      Console.WriteLine($"После DramaAction:");
+      Console.WriteLine(morrigan.GetInfo());
       Console.WriteLine(merlin.GetInfo());
       Console.WriteLine();
 
-      Console.WriteLine($"{merlin.Name} снова атакует {lancelot.Name}!");
-
-      beforeHP = lancelot.HP;
-      merlin.Hit(lancelot);
-      damage = beforeHP - lancelot.HP;
-
-      critText = merlin.LastHitWasCrit ? " – КРИТИЧЕСКИЙ УДАР!" : "";
-
-      Console.WriteLine($"Нанесено {damage} урона{critText}\n");
-
+      // ИТОГОВОЕ СОСТОЯНИЕ
       Console.WriteLine("ИТОГОВОЕ СОСТОЯНИЕ:");
       Console.WriteLine("==========================");
-      Console.WriteLine(lancelot.GetInfo());
+      Console.WriteLine(morrigan.GetInfo());
       Console.WriteLine(merlin.GetInfo());
 
       Console.ReadKey();
