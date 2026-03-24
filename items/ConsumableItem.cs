21f504de093226f6120978f8d748b5fa832e310f
neldori <nexusstail@gmail.com>
2026-03-24 20:14:59 +0700

    разпределил по папкам

---
+++ items/ConsumableItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoringRPG {
+  internal abstract class ConsumableItem {
+    public int Value { get; set; }
+
+    public ConsumableItem(int value) {
+      Value = value;
+    }
+  }
+}
